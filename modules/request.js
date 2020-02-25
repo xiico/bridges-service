b2afa3f225bc7c1a59b0bbdbbd863a7203d03871
francisco marão <francisco.marao@hotmail.com>
2020-02-25 08:34:26 -0300

    first commit

---
+++ modules/request.js
@@ -0,0 +1,36 @@
+var https;// = require('https');
+var http = require('http');
+module.exports = {
+    get: function (path, callBack) {
+        (https || http).get({
+            host: process.env.HOST_URL,
+            path: path,
+            port: 3000,
+            headers: { 'authorization': `Bearer ${process.env.SERVER_KEY}` }        
+        }, function (res) {
+            // explicitly treat incoming data as utf8 (avoids issues with multi-byte chars)
+            res.setEncoding('utf8');
+
+            // incrementally capture the incoming response body
+            var body = '';
+            res.on('data', function (d) {
+                body += d;
+            });
+
+            // do whatever we want with the response once it's done
+            res.on('end', function () {
+                try {
+                    var parsed = JSON.parse(body);
+                } catch (err) {
+                    console.error('Unable to parse response as JSON', err);
+                    return callBack(err);
+                }
+                callBack(null, parsed);
+            });
+        }).on('error', function (err) {
+            // handle errors with the request itself
+            console.error('Error with the request:', err.message);
+            callBack(err);
+        }).end();
+    }
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+var  schedule  =  require('node-schedule');
+require('dotenv').config({ path: __dirname + '/.env' });
+var log = require('./modules/log');
+var request = require('./modules/request');
+
+console.log(log.timeStamp() + " service starting...");
+
+var everyHour = schedule.scheduleJob('5 */1 * * *', function () {
+    request.get('/evaluationnotification', (err,result) => {
+        if(err) return console.error(log.timeStamp() + ' err:', err);
+        console.log(log.timeStamp() + ' eval:', result);
+    });
+});
+
+var everyTwenty = schedule.scheduleJob('*/20 * * * *', function () {
+    request.get('/testapi/luckynumber', (err,result) => {
+        if(err) return console.error(log.timeStamp() + ' err:', err);
+        console.log(log.timeStamp() + ' lucky:', result);
+    });
+});
+
+var everyDay1 = schedule.scheduleJob('0 8 * * *', function () {
+    console.log(log.timeStamp() + ' reminder:', result);
+});
+
+// var everySixHour = schedule.scheduleJob('5 */6 * * *', function () {
+//     db.clanUpdate();
+// });
+
+// var everyDay1 = schedule.scheduleJob('10 0 * * *', function () {
+//     db.playerUpdate();
+// });
+
+// var everyDay2 = schedule.scheduleJob('15 0 * * *', function () {    
+//     db.countryRankUpdate();
+// });
+
+console.log(log.timeStamp() + " service started...");
